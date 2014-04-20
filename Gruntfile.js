54eb43de0b6960117b48d6206ab6e6d3e6d76e25
Philip Ford <paf1687@gmail.com>
2014-04-20 16:31:32 -0400

    AUR-281:  Plotted points, and can plot them as icons.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,27 +6,41 @@
 
 module.exports = function(grunt){
 
+    var server = require("./server.js");
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        war: {
-            target: {
-                options: {
-                    war_dist_folder: 'target',
-                    war_verbose: true,
-                    war_name: 'cesium-test',
-                    webxml_welcome: 'index.html',
-                    webxml_display_name: 'Cesium Test'
-                },
-                files: [
-                    {
-                        expand: true,
-                        src: ['src/**']
-                    }
-                ]
+        server: {
+            port: 9000
+        },
+        watch: {
+            scripts: {
+                files: ['**/*.js', '**/*.json', '!**/node_modules/**']
+            },
+            css: {
+                files: ['**/*.scss'],
+                task: ['sass']
+            },
+            html: {
+                files: ['**/*.html']
+            },
+            options: {
+                livereload: true
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'src/css/main.css' : 'src/sass/main.scss'
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-war');
-    grunt.registerTask('default', ['war']);
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('server', 'Starts the development server.', function() {
+        server.start(grunt.config("server.port"));
+    });
+    grunt.registerTask('default', ['server', 'sass', 'watch'])
 };
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 
 var express = require("express"),
-    app = express(),
-    port = 8090;
+    app = express();
 
 app.configure(function(){
     app.use(express.methodOverride());
     app.use(express.bodyParser());
-    app.use("/cesium-test", express.static(__dirname + "/src/"));
+    app.use("/cesium-example", express.static(__dirname + "/src/"));
     app.use(app.router);
 });
 
-app.listen(port);
-console.log("Server listening on port " + port);
+exports.start = function(port){
+    app.listen(port);
+    console.log("Server listening on port " + port);
+}
